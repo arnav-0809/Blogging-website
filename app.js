b9edb1fad438b985616ecf2196ab2ea7dfdd6451
Arnav Sehgal <arnavsehgal2001@gmail.com>
2021-07-08 02:41:03 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-// require('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
